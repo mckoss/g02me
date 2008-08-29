0477f5be6460c50333ff28dbb0f6e0db993613a6
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2008-08-29 22:58:11 +0000

    Base 64 working

--- trunk/app/map.py
+++ trunk/app/map.py
@@ -2,32 +2,38 @@ from google.appengine.ext import db
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from google.appengine.api import memcache
+import util
 
 import logging
 from sys import exc_info
 
 class Map(db.Model):
-    id = db.IntegerProperty()
-    url = db.StringProperty()
-    title = db.StringProperty()
-    dateCreated = db.DateProperty()
+    url = db.StringProperty(required=True, validator=util.NormalizeUrl)
+    title = db.StringProperty(validator=util.TrimString)
+    dateCreated = db.DateProperty(auto_now=True)
     
+    @classmethod
+    def KeyFromId(self, id):
+        return "K:%s" % id
+
     @classmethod
     def Lookup(cls, id):
         # just use id as key - more frequent than url lookup
-        query = db.Query(Map)
-        query.filter('id =', int(id))
-        map = query.get()
+        map = Map.get_by_key_name(Map.KeyFromId(id))
         return map
     
     @classmethod
     def FindUrl(cls, url):
+        url = util.NormalizeUrl(url)
         query = db.Query(Map)
         query.filter('url =', url)
         map = query.get()
         return map
+    
+    def GetId(self):
+        return self.key().name()[2:]
 
-# TODO: Use a sharded counter - see Google IO video     
+# TODO: Use a sharded counter - see Google I/O video     
 class Globals(db.Model):
     idNext = db.IntegerProperty(default=1)
     
@@ -37,32 +43,35 @@ class Globals(db.Model):
         id = glob.idNext
         glob.idNext = glob.idNext + 1
         glob.put()
-        return id
+        return util.IntToS64(id)
 
 def MakeAlias(req):
     url = req.GET["url"]
     map = Map.FindUrl(url)
     if map == None:
         if req.has_key("title"):
-            title = req.GET["title"]
+            title = req.GET["title"] or ""
         id = Globals.IdNext()
-        map = Map(id=id, url=url, title=title)
+        logging.info("ID: %s" % id)
+        map = Map(key_name="K:%s"%id, url=url, title=title)
         map.put()
-    return HttpResponseRedirect("/%s" % str(map.id))
+    return HttpResponseRedirect("/%s" % map.GetId())
 
 def Head(req):
     id = req.GET["id"]
     map = Map.Lookup(int(id))
     if map == None:
         return render_to_response('error.html', {'strError' : "No such id: %s" % id})
-    return render_to_response('head.html', {'id':map.id, 'url':map.url, 'title':map.title})
+    return render_to_response('head.html', {'id':map.GetId(), 'url':map.url, 'title':map.title})
 
 def FrameSet(req, id):
     logging.info(id)
     map = Map.Lookup(int(id))
     if map == None:
         return render_to_response('error.html', {'strError' : "No such id: %s" % id})
-    return render_to_response('mapped.html', {'id':map.id, 'url':map.url, 'title':map.title})
+    return render_to_response('mapped.html', {'id':map.GetId(), 'url':map.url, 'title':map.title})
+
+        
 
     
     
--- trunk/app/urls.py
+++ trunk/app/urls.py
@@ -7,5 +7,4 @@ urlpatterns = patterns('',
     (r'^map.*', map.MakeAlias),
     (r'^(\d+)', map.FrameSet),
     (r'^info.*', map.Head),
-    
 )
