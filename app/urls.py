9e7d9b5123eb9c12a7ec78812570e78fdf4fc52d
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2008-09-02 18:50:46 +0000

    Refactor map to views and models

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
-import map
+import views
 
 urlpatterns = patterns('',
-    (r'^$', map.Home),
-    (r'^\+map.*', map.MakeAlias),
-    (r'^\+info.*', map.Head),
-    (r'^([0-9A-Za-z-_]+)', map.FrameSet),
+    (r'^$', views.Home),
+    (r'^\+map.*', views.MakeAlias),
+    (r'^\+info.*', views.Head),
+    (r'^([0-9A-Za-z-_]+)', views.FrameSet),
 )
---
+++ app/views.py
@@ -0,0 +1,44 @@
+from google.appengine.ext import db
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from google.appengine.api import memcache
+import util
+from models import *
+
+import logging
+from sys import exc_info
+
+def Home(req):
+    InitReq(req)
+    return render_to_response('home.html', {'host':util.local.stHost})
+
+def MakeAlias(req):
+    url = req.GET["url"]
+    map = Map.FindUrl(url)
+    if map == None:
+        if req.has_key("title"):
+            title = req.GET["title"] or ""
+        id = Globals.IdNext()
+        map = Map(key_name="K:%s"%id, url=url, title=unicode(title, 'utf8'))
+        map.put()
+    return HttpResponseRedirect("/%s" % map.GetId())
+
+def Head(req):
+    InitReq(req)
+    id = req.GET["id"]
+    map = Map.Lookup(id)
+    if map == None:
+        return render_to_response('error.html', {'strError' : "The G02.ME page, http://g02.me/%s does not exist" % id})
+    return render_to_response('head.html', map.GetDict())
+
+def FrameSet(req, id):
+    InitReq(req)
+    map = Map.Lookup(id)
+    if map == None:
+        return render_to_response('error.html', {'strError' : "The G02.ME page, http://g02.me/%s does not exist" % id})
+    return render_to_response('mapped.html', map.GetDict())
+
+def InitReq(req):
+    # Store the http request for URI generation, in a thread local
+    # TODO: Put in MiddleWare?
+    util.local.stHost = "http://" + req.META["HTTP_HOST"] + "/"
