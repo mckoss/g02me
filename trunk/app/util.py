f2fe3e1593905412a24326ba51434f7e33d0c45d
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2008-11-23 08:12:50 +0000

    Make Age a template filter instead of a model/comment method

--- trunk/app/util.py
+++ trunk/app/util.py
@@ -487,38 +487,6 @@ def RunInTransaction(func):
     return _transaction
 
 # --------------------------------------------------------------------
-# String utilities - format date as an "age"
-# --------------------------------------------------------------------
-
-def SAgeReq(dt):
-    # Return the age (time between time of request and a date) as a string
-    return SAgeDdt(local.dtNow - dt)
-
-def SAgeDdt(ddt):
-    if ddt.days < 0:
-        return "future?"
-    months = int(ddt.days*12/365)
-    years = int(ddt.days/365)
-    if years >= 1:
-        return "%d year%s ago" % (years, SPlural(years))
-    if months >= 3:
-        return "%d months ago" % months 
-    if ddt.days == 1:
-        return "yesterday"
-    if ddt.days > 1:
-        return "%d days ago" % ddt.days
-    hrs = int(ddt.seconds/60/60)
-    if hrs >= 1:
-        return "%d hour%s ago" % (hrs, SPlural(hrs))
-    minutes = round(ddt.seconds/60)
-    if minutes < 1:
-        return "seconds ago"
-    return "%d minute%s ago" % (minutes, SPlural(minutes))
-
-def SPlural(n, sPlural="s", sSingle=''):
-    return [sSingle, sPlural][n!=1]
-
-# --------------------------------------------------------------------
 # Signed and verified strings can only come from the server
 # --------------------------------------------------------------------
 
