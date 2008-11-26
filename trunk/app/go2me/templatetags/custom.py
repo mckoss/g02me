ab5cbf2c84370e8c076f64ce126a135b81493d7b
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2008-11-26 22:46:49 +0000

    with tag import from Django 1.0

--- trunk/app/go2me/templatetags/custom.py
+++ trunk/app/go2me/templatetags/custom.py
@@ -69,6 +69,10 @@ def SAgeDdt(ddt):
 def SPlural(n, sPlural="s", sSingle=''):
     return [sSingle, sPlural][n!=1]
 
+# --------------------------------------------------------------------
+# setvar tag from Django issue: http://code.djangoproject.com/ticket/1322
+# --------------------------------------------------------------------
+
 class SetVariable(template.Node):
     def __init__(self, varname, nodelist):
         self.varname = varname
@@ -96,4 +100,46 @@ def setvar(parser, token):
     parser.delete_first_token()
     return SetVariable(varname, nodelist)
 
+# --------------------------------------------------------------------
+# with tag back-ported from Django 1.0
+# --------------------------------------------------------------------
+
+class WithNode(template.Node):
+    def __init__(self, var, name, nodelist):
+        self.var = var
+        self.name = name
+        self.nodelist = nodelist
+
+    def __repr__(self):
+        return "<WithNode>"
+
+    def render(self, context):
+        val = self.var.resolve(context)
+        context.push()
+        context[self.name] = val
+        output = self.nodelist.render(context)
+        context.pop()
+        return output
+
+@register.tag(name='with')
+def do_with(parser, token):
+    """
+    Adds a value to the context (inside of this block) for caching and easy
+    access.
+
+    For example::
+
+        {% with person.some_sql_method as total %}
+            {{ total }} object{{ total|pluralize }}
+        {% endwith %}
+    """
+    bits = list(token.split_contents())
+    if len(bits) != 4 or bits[2] != "as":
+        raise TemplateSyntaxError("%r expected format is 'value as name'" %
+                                  bits[0])
+    var = parser.compile_filter(bits[1])
+    name = bits[3]
+    nodelist = parser.parse(('endwith',))
+    parser.delete_first_token()
+    return WithNode(var, name, nodelist)
 
