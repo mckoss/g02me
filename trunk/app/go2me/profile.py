f270078dfca2f40099309fc615720d1beef849a8
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2008-11-29 01:01:51 +0000

    checkpoint - user accounts

--- trunk/app/go2me/profile.py
+++ trunk/app/go2me/profile.py
@@ -15,18 +15,20 @@ class Profile(db.Model):
     User Profile information for a logged-in user
     """
     ss = ScoreSet.GetSet("karma", [hrsWeek, hrsMonth])
+    regUsername = re.compile(r"^[a-zA-Z0-9_\.\-]{1,20}$")
     
     # Account identifiers
-    user = db.UserProperty()                # Google account
-    username = db.StringProperty()          # Go2.me nickname
-    keyCrypt = db.StringProperty()               # Per-user encryption key
+    user = db.UserProperty(required=True)               # Google account
+    username = db.StringProperty(required=True)         # Go2.me nickname
+    keyCrypt = db.StringProperty()                      # Per-user encryption key
     dateCreated = db.DateTimeProperty()
     userTwitter = db.StringProperty()
-    passTwitterCipher = db.StringProperty() # E(key, passPlain)
-    keyFriendFeed = db.StringProperty()     # API Key for Friendfeed
-    userAuthFirst = db.StringProperty()     # Initial auth token for the creating user
+    passTwitterCipher = db.StringProperty()             # E(key, passPlain)
+    keyFriendFeed = db.StringProperty()                 # API Key for Friendfeed
+    userAuthFirst = db.StringProperty()                 # Initial auth token for the creating user
 
-    fBan = db.BooleanProperty(default=False)
+    fBanned = db.BooleanProperty(default=False)
+    fAdmin = db.BooleanProperty(default=False)
     
     # Personal/profile information
     dateBirth = db.DateProperty()
@@ -42,14 +44,27 @@ class Profile(db.Model):
 
     fBan = db.BooleanProperty(default=False)
     
-    @classmethod
-    def Create(cls):
-        local.requser.Require('write', 'user')
-        profile = Profile(user=local.requser.user, username=local.requser.username, userAuthFirst=local.requser.UserId(),
-                  dateCreated=local.dtNow)
-        return profile
+    @staticmethod
+    def FindOrCreate(user, username):
+        # First try lookup by username (and create if not taken)
+        profile = None
+        if username:
+            Profile.RequireValidUsername(username)
+            profile = Profile.get_or_insert(key_name='U:' + username, user=user, username=username, userAuthFirst=local.requser.UserId(),
+                      dateCreated=local.dtNow)
 
-    @classmethod
-    def Lookup(cls, user):
-        profile = Profile.gql('WHERE user = :1', user).get()
+        if not profile or profile.user != user:
+            # username and user do not match - switch to the user's account - ignore username
+            profile = Profile.gql('WHERE user = :1', user).get()
         return profile
+    
+    @staticmethod
+    def Lookup(username):
+        profile = Profile.gql('WHERE username = :1', username).get()
+        return profile
+    
+    @staticmethod
+    def RequireValidUsername(username):
+        if not Profile.regUsername.match(username):
+            raise Error("Invalid nickname: %s" % username, 'Fail/Auth')
+
