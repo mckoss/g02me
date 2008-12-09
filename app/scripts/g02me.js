9c08c6b7c85b00fda72baa951bdff6c26baf020f
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2008-12-09 22:07:43 +0000

    checkpoint - profile edits, saved indicator, sign out redirects

--- app/scripts/g02me.js
+++ app/scripts/g02me.js
@@ -32,7 +32,10 @@ SetUsername: function(sUsername)
 			{
 		case 'OK':
 			// Refresh the page to reset the display for the new server-set cookie
-			window.location.href = window.location.href;
+			if (obj.username == '')
+				window.location.href = "/";
+			else
+				window.location.href = window.location.href;
 			break;
 		case 'Fail/Auth/Used':
 			if (confirm("The nickname, " + sUsername + ", is already in use.  Are you sure you want to use it?"))
