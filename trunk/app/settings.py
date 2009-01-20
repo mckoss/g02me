f5bfd93811fb18454c2d42ccfe180bf3e614ec62
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2009-01-20 22:44:15 +0000

    twitter share link

--- trunk/app/settings.py
+++ trunk/app/settings.py
@@ -23,6 +23,7 @@ ADMINS = (
 # Version 5 (base 57) starts at '32' (since there is no 0 or 1 in the alphabet)
 sSiteName = "Go2.me"
 sSiteHost = "go2.me"
+sTwitterSource = "go2me"
 sSiteTitle = "A Link Shortening Service"
 sSiteTagline = "Don't just send a link - start a conversation."
 nLinkLength = len("http://%s/" % sSiteHost) + 2 
