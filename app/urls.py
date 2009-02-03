ab555096f7aaeaff39ec2f21bc3feb1792d5be52
mckoss <mckoss@c0443566-8655-0410-b78e-4b5510a4d118>
2009-02-03 03:13:12 +0000

    Heading changes

--- app/urls.py
+++ app/urls.py
@@ -3,17 +3,25 @@ from go2me.views import *
 
 urlpatterns = patterns('',
     (r'^$', Home),
-    (r'^info/([0-9A-Za-z-_]+)$', Head),
+    (r'^popular/$', Popular),
+
     (r'^user/([a-zA-Z0-9_\.\-]{1,20})$', UserView),
     (r'^user/([a-zA-Z0-9_\.\-]{1,20})/picture_(thumb|med|full)$', UserPicture),    
-    (r'^profile$', UserProfile),    
     (r'^tag/([0-9A-Za-z-_]+)$', TagView),
-    (r'^init/$', InitAPI),
+
+    (r'^profile/$', UserProfile),    
+
     (r'^map/$', MakeAlias),
-    (r'^lookup/$', Lookup),
-    (r'^([0-9A-Za-z-_]+)$', FrameSet),
-    (r'^cmd/setusername$', SetUsername),
     (r'^comment/(?P<command>[a-z\-]+)?$', DoComment),
+    (r'^init/$', InitAPI),
+    (r'^lookup/$', Lookup),
+    (r'^cmd/setusername$', SetUsername),
     (r'^admin/(?P<command>[a-z\-]+)?$', Admin),
+    
+    (r'^([0-9A-Za-z-_]+)$', FrameSet),
+    
+    # Archaic
+    (r'^info/([0-9A-Za-z-_]+)$', HeadRedirect),
+
     (r'^.*$', CatchAll),
 )
